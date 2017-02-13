a2e503253737452f35257c23d48d324eef5aeea8
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-13 12:38:34 -0800

    completed selection sort, however, trying to implement without builtin min function

--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,13 @@
-def selection_sort(array):
-    sorted_array = []
-    for j in range(len(array)):
-        for i in range(len(array)-1):
-            if array[i] < array[i+1]:
-                new_min = array[i]
-            if array[i+1] < new_min:
-                new_min = array[i+1]
-        sorted_array.append(new_min)
-    return sorted_array
+def selection_sort(source):
+    for i in range(len(array)):
+        minimum = min(array[i:])
+        min_index = array[i:].index(minimum)
+        array[i + min_index], array[i] = array[i], array[i + min_index]
+    return array
 
 if __name__ == "__main__":
     import random
-    # array = [random.randint(-1000,1000) for _ in range(100)]
-    array = [4,5,8,10,16,1,-1000,14,6,-5,-420]
+    array = [random.randint(-1000,1000) for _ in range(100)]
     print "\nUnsorted Array: {}\n".format(array)
     sorted_array = selection_sort(array)
     print "\nSorted Array: {}\n".format(sorted_array)
