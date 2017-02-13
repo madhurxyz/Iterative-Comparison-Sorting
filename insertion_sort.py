383476128e3566dec2f4fe04f0a32387785a55ca
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-13 11:16:43 -0800

    completed insertion sort, negative and repeated numbers test passed

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,13 +1,17 @@
 def insertion_sort(array):
-    first = array[0]
-    new_array = []
-    new_array.append(first)
     for i in range(1, len(array)):
-        if array[i]>new_array[i]:
-            new_array.append(array[i])
+        item = array[i]
+        j = i
+        while j > 0 and array[j-1] > item:
+            array[j] = array[j-1]
+            j = j - 1
+        array[j] = item
+    print "\nSorted Array: {}\n".format(array)
+    return array
 
 
 if __name__ == "__main__":
     import random
     array = [random.randint(-1000,1000) for _ in range(100)]
+    print "\nUnsorted Array: {}\n".format(array)
     insertion_sort(array)
