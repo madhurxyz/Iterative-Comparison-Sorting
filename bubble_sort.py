b5d5ab4fe642c295cfa42ae69276dfd73e9bbc7a
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-13 10:33:54 -0800

    negative numbers, and repeated numbers tests passed

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,5 +11,5 @@ def bubble_sort(array):
 
 if __name__ == "__main__":
     import random
-    array = [random.randint(1,100) for _ in range(1000)]
+    array = [random.randint(-1000,1000) for _ in range(100)]
     bubble_sort(array)
