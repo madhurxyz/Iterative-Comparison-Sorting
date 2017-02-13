ec8934544f7fa9fe030c5e8ae74f0ec056e971c1
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-13 10:21:46 -0800

    completed bubble sort implementation

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,15 @@
 def bubble_sort(array):
-    
-if __name__ = "__main__":
-    array = [1,4,5,18,99,87,32,45,67,80]
+    is_sorted = True
+    while is_sorted:
+        is_sorted = False
+        for i in range(len(array)-1):
+            if array[i]>array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                is_sorted = True
+    print array
+    return array
+
+if __name__ == "__main__":
+    import random
+    array = [random.randint(1,100) for _ in range(1000)]
     bubble_sort(array)
