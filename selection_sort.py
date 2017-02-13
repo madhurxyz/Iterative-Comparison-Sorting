ac363ec372299c0e08d05a2db11922b89ad32adc
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-13 11:33:55 -0800

    made progress on selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,9 +1,16 @@
 def selection_sort(array):
-    
+    for i in range(len(array)-1):
+        if array[i] < array[i+1]:
+            new_min = array[i]
+        if array[i+1] < new_min:
+            new_min = array[i+1]
+            print new_min
+
 
 if __name__ == "__main__":
     import random
-    array = [random.randint(-1000,1000) for _ in range(100)]
+    # array = [random.randint(-1000,1000) for _ in range(100)]
+    array = [4,5,8,10,16,1,14,6,-5,-420]
     print "\nUnsorted Array: {}\n".format(array)
     sorted_array = selection_sort(array)
     print "\nSorted Array: {}\n".format(sorted_array)
